4aff4a41f2d4d243eba72098c98fada06af41d43
omidespargham <omidespargham2001@gmail.com>
2023-03-05 09:14:56 +0330

    make advert and get all adverts path function

--- routers/advert/advert_v1.py
+++ routers/advert/advert_v1.py
@@ -5,8 +5,22 @@ from sqlalchemy.orm import Session
 from db.database import get_db
 from db import user_db
 from models.user_model import RGScode, UserDB
+from models.advert_models import Advert
 from auth.oauth2 import create_access_token
 
 router = APIRouter(prefix="/v1", tags=["advert_v1"])
 
 
+@router.post("/make-advert",response_model=schema.AdvertShow)
+def make_advert(request:schema.AdvertBase,db:Session=Depends(get_db)):
+    advert = Advert(title=request.title,description=request.description,price=request.price,
+                    user_id=request.user_id,phone_number=request.phone_number)
+    db.add(advert)
+    db.commit()
+    db.refresh(advert)
+    return advert
+
+@router.get("/get_all_adverts/{user_id}",response_model=List[schema.AdvertShow])
+def get_all_adverts(user_id:int,db:Session = Depends(get_db)):
+    adverts = db.query(Advert).filter(Advert.user_id == user_id).all()
+    return adverts
